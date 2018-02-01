eba1ecff9840e19d63a55ce7df0323f29e702be1
Scott Forman <sjforman@gmail.com>
2018-01-31 18:20:17 -0800

    Listens for esc, spacebar, and tab in editing tweets.

--- src/Tweet.js
+++ src/Tweet.js
@@ -8,13 +8,42 @@ export class Tweet extends React.Component {
     this.state = {
       editMode: false 
     }
-    this.toggleEditMode = this.toggleEditMode.bind(this);
+    this.enterEditMode = this.enterEditMode.bind(this);
+    this.escFunction = this.escFunction.bind(this);
+    this.spaceFunction = this.spaceFunction.bind(this);
   }
   
-  toggleEditMode() {
+  escFunction(event){
+    if(event.keyCode === 27) {
+      this.setState({
+        editMode : false
+      });
+      this.props.handleTweetSubmit()
+    }
+  }
+
+  spaceFunction(event){
+    if(event.keyCode === 32) {
+      this.setState({
+        editMode : true
+      });
+    }
+  }
+
+  enterEditMode() {
     this.setState({
-      editMode : !this.state.editMode
+      editMode : true
     });
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.escFunction, false);
+    document.addEventListener("keydown", this.spaceFunction, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.escFunction, false);
+    document.removeEventListener("keydown", this.spaceFunction, false);
   }
 
   render() {
@@ -30,22 +59,25 @@ export class Tweet extends React.Component {
         {this.state.editMode ? 
 
               <div>
-                <textarea className="w-100 f3 db border-box hover-black ba b--black-20 pa2 br2 mb2 h4" value={this.props.text} onChange={this.props.handleChange}/>
+                <textarea 
+                  className="w-100 f3 db border-box hover-black ba b--black-20 pa2 br2 mb2 h4" 
+                  value={this.props.text} 
+                  onChange={this.props.handleChange} 
+                  onKeyDown={this.escFunction}
+                />
                 <p>{this.props.text.length} / {maxLength} ({maxLength - this.props.text.length} remaining)</p>
               </div>
 
           :
 
-            <div className="f2">
+            <div className="w-100 f3 db border-box hover-black b--black-20 pa2 br2 mb2 h4" onClick={this.enterEditMode} onKeyDown={this.spaceFunction} tabIndex="0">
               {this.props.text}
             </div>
         }
       </div>
 
         <div className="fl w-100 w-10-ns pa2">
-          <button className="f6 link dim br1 ba bw1 ph3 pv2 mb2 dib mid-gray" onClick={this.toggleEditMode}>Edit</button>  
           <button className="f6 link dim br1 ba bw1 ph3 pv2 mb2 dib mid-gray" onClick={this.props.deleteTweet.bind(this)}>Delete</button>
-          <button className="f6 link dim br1 ba bw1 ph3 pv2 mb2 dib mid-gray" onClick={this.props.handleTweetSubmit}>Save</button>
         </div>
       </div>
       </div>
