b8ba788d1137dfc68749a6e5964631afb73817d6
Scott Forman <sjforman@gmail.com>
2018-03-01 10:13:20 -0800

    Changes name to Threadmaker.

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ export class Header extends Component {
         <div className="fl w-75 pa2 v-mid">
           <img src={logo} className="dib w2 v-mid ph3" alt="logo" />
           <a href="/dashboard" className="link black bg-animate hover-blue">
-            <h1 className="lh-title dib v-mid">Threadbuilder</h1>
+            <h1 className="lh-title dib v-mid">Threadmaker</h1>
           </a>
         </div>
         <div className="fl tr w-25 mt3 pt2 pr4">
--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ export class Home extends Component {
 
         <div className="tc pt5 pb3">
           <img src={logo} alt="logo" className="dib w3 v-mid pr1" />
-          <h1 className="dib f3 f2-m f1-l fw2 black-90 mv3 v-mid">Threadbuilder</h1>
+          <h1 className="dib f3 f2-m f1-l fw2 black-90 mv3 v-mid">Threadmaker</h1>
           <h2 className="f5 f4-m f3-l fw2 black-50 mt0 lh-copy">
             The easiest way to compose, edit, and publish Twitter threads.
           </h2>
@@ -17,7 +17,7 @@ export class Home extends Component {
 
       <div className="mr-auto ml-auto w-60-ns w-100 pa2">
         <p className="f5 f4-ns lh-copy tj">
-        With Threadbuilder, you can draft, save, edit, and reorder the tweets in a thread until you’re satisfied. Then click ‘publish’ to send ’em all to Twitter, properly threaded and with automatic numbering.
+        With Threadmaker, you can draft, save, edit, and reorder the tweets in a thread until you’re satisfied. Then click ‘publish’ to send ’em all to Twitter, properly threaded and with automatic numbering.
         </p>
       </div>
 
